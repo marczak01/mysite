35b9c856742ecef33224f49fa26e30c945711c76
Marek Marczak <marekmarczak@MacBook-Air-Marek.local>
2025-09-03 18:09:32 +0200

    search functionality

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.postgres.search import SearchVector
 from taggit.models import Tag
 from django.core.mail import send_mail
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 # from django.http import Http404
 from django.db.models import Count
 from .models import Post, Comment
@@ -109,3 +110,20 @@ def post_comment(request, post_id):
     return render(request, 'blog/post/comment.html', {'post': post,
                                                       'form': form,
                                                       'comment': comment})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+
+    return render(request, 'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
